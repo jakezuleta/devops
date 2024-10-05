054af456fb5125a3fda4f685efc34b0106f8f7b9
jakezuleta <68143656+jakezuleta@users.noreply.github.com>
2024-10-05 10:46:11 +0800

    Add files via upload

---
+++ configcisco2.py
@@ -0,0 +1,33 @@
+import json
+import netmiko
+from netmiko import ConnectHandler
+
+# Parse the Data from Json file to python Var
+with open('configcisco1.json') as config_file:
+    config = json.load(config_file)
+    
+# Extract the configuraton data:
+device_info = config['device']
+interface_config = config['interface_config']
+new_hostname = config['new_hostname']
+
+# Connect to Cisco via SSH
+net_connect = ConnectHandler(**device_info)
+
+# Enter Enable mode
+net_connect.enable()
+
+#Build the Configuration: 
+commands = [
+    f"interface {interface_config['interface']}",
+    f"ip address {interface_config['new_ip']} {interface_config['subnet_mask']}",
+    "no shutdown", #make sure its UP
+    "exit",
+    f"hostname {new_hostname}"
+]
+# send the json Parsed Python file Cisco
+output = net_connect.send_config_set (commands)
+print(output)
+
+# always close the connection:
+net_connect.disconnect
